5c3824816ab71765b7f488bc2faba3b89713d40e
AdamBorg <adam.borg@my.jcu.edu.au>
2016-10-01 14:15:14 +1000

    Intialise Prac07

---
+++ Prac07/usedCars.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 Practical
+Client code to use the Car class
+Note that the import has a folder (module) in it.
+"""
+from Prac07.car import Car
+
+
+def main():
+    bus = Car(180 ,"Bus")
+    bus.drive(30)
+    limo = Car(100, "Limo")
+    limo.add_fuel(20)
+    limo.drive(115)
+    print(limo.fuel)
+    print(limo.odometer)
+    print("fuel =", bus.fuel)
+    print("odo =", bus.odometer)
+    print(bus)
+
+    # print("Car {}, {}".format(bus.fuel, bus.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=bus))
+
+main()
